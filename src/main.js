5fc464dd55decade78f286b13f12c734f26ea767
大青虫 <46221967+daqingchong0809@users.noreply.github.com>
2020-07-30 15:42:55 +0800

    模板1.0

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+// 内部引入
+import "./assets/css/reset.css";
+
+// 外部引入
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+Vue.config.productionTip = false;
+
+Vue.use(Antd);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
